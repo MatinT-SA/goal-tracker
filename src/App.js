e75e1806ac47feab9a48e521256fea1565ecb6cd
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-12-02 14:11:19 +0330

    componenets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 export default function App() {
   return (
     <div>
-      <h1>goal tracker</h1>
+      <Header />
     </div>
   );
+}
+
+function Header() {
+  return <div>Header</div>;
+}
+
+function GoalForm() {
+  return <div>form goal</div>;
+}
+
+function GoalList() {
+  return <div>goal list</div>;
+}
+
+function GoalItem() {
+  return <div>goal item</div>;
+}
+
+function ProgressBar() {
+  return <div>Progress bar</div>;
+}
+
+function Filter() {
+  return <div>filter</div>;
+}
+
+function SearchBar() {
+  return <div>search bar</div>;
+}
+
+function Modal() {
+  return <div>Modal</div>;
+}
+
+function Footer() {
+  return <div>footer</div>;
 }
