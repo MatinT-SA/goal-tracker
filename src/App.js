5008bc40e890f7978dd76092e62c81da1249dcd7
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-12-03 16:42:39 +0330

    adding background

--- src/App.js
+++ src/App.js
@@ -7,7 +7,21 @@ export default function App() {
 }
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div class="custom-shape-divider-top-1733231526">
+      <svg
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1200 120"
+        preserveAspectRatio="none"
+      >
+        <path
+          d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z"
+          class="shape-fill"
+        ></path>
+      </svg>
+    </div>
+  );
 }
 
 function GoalForm() {
