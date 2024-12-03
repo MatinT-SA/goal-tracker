7fed55ddd7da9f86ad26b48a62c8c6fcbade6123
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-12-03 17:34:21 +0330

    header

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { FaSearch } from "react-icons/fa";
+
 export default function App() {
   return (
     <div>
@@ -8,19 +10,32 @@ export default function App() {
 
 function Header() {
   return (
-    <div class="custom-shape-divider-top-1733231526">
-      <svg
-        data-name="Layer 1"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1200 120"
-        preserveAspectRatio="none"
-      >
-        <path
-          d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z"
-          class="shape-fill"
-        ></path>
-      </svg>
-    </div>
+    <header className="header">
+      <img src="images/logo.png" alt="Logo" />
+
+      <nav className="header-nav">
+        <div className="search-box">
+          <input type="text" placeholder="Search..." />
+          <button>
+            <FaSearch className="search-icon" />
+          </button>
+        </div>
+      </nav>
+
+      <div class="custom-shape-divider-top-1733231526">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+    </header>
   );
 }
 
