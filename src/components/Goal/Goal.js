98ad561a239d69b10bdfc1bd45c5207849554c2d
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-12-06 18:44:44 +0330

    fixing dueDate and addedDate

--- src/components/Goal/Goal.js
+++ src/components/Goal/Goal.js
@@ -8,15 +8,13 @@ export function Goal({ goal }) {
       <div className={`goal-container ${priorityClass}`}>
         <div className="priority-ribbon"></div>
         <h3 className={`goal-name ${checked ? "checked" : ""}`}>{goalName}</h3>
-        <p className="dates">
-          <span className="added-date">
-            Added: {new Date(addedDate).toLocaleDateString()}
-          </span>
-          <span className="due-date">
-            Due: {new Date(dueDate).toLocaleDateString()}
-          </span>
-        </p>
+        <span className="added-date">
+          Added: {new Date(addedDate).toLocaleDateString()}
+        </span>
         {notes && <p className="notes">{notes}</p>}
+        <span className="due-date">
+          Due: {new Date(dueDate).toLocaleDateString()}
+        </span>
       </div>
     </li>
   );
