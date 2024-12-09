a29bdd8b6a14def43a6beb775f278aba88c364a0
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-12-09 16:36:22 +0330

    calendar icon for datepicker

--- src/components/Goal/GoalForm.js
+++ src/components/Goal/GoalForm.js
@@ -32,14 +32,28 @@ export function GoalForm({ onAddGoal }) {
     setNotes("");
   }
 
-  const CustomDateInput = forwardRef(({ value, onClick, className }, ref) => (
+  const CustomDateInput = forwardRef(({ onClick }, ref) => (
     <button
       type="button"
       onClick={onClick}
       ref={ref}
-      className={`custom-date-button ${className}`}
+      className="custom-date-picker-button"
+      aria-label="Open calendar"
     >
-      {value || "Select due date and time"}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        className="calendar-icon"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+        />
+      </svg>
     </button>
   ));
 
