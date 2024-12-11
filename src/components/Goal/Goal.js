f8fed14e7bbadd9ba3401a73c644ab5da27c26b6
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-12-11 21:28:41 +0330

    making some more adjustments after adding smooth animation pagination

--- src/components/Goal/Goal.js
+++ src/components/Goal/Goal.js
@@ -12,10 +12,10 @@ export function Goal({ goal }) {
   return (
     <motion.li
       className="goal"
-      initial={{ opacity: 0, x: -20 }}
+      initial={{ opacity: 0, x: -50 }}
       animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: 20 }}
-      transition={{ duration: 0.3 }}
+      exit={{ opacity: 0, x: 50 }}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
     >
       <div className={`goal-container ${priorityClass}`}>
         <IoMdRocket className="icon-goal" />
