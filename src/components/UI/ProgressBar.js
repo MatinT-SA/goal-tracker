7fa6cd21477f0fd16b97ee2701e08babb3ff8bc6
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-12-15 18:10:24 +0330

    custom box-shadow and background color for progress-bar and its result when done

--- src/components/UI/ProgressBar.js
+++ src/components/UI/ProgressBar.js
@@ -3,14 +3,19 @@ import { useSpring, animated } from "@react-spring/web";
 function ProgressBar({ progress }) {
   const props = useSpring({ width: `${progress}%` });
 
+  const customBackgroundColor = progress === 100 ? "#4caf50" : "#03424c";
+
+  const progressClass = progress === 100 ? "completed" : "";
+
   return (
-    <div className="progress-bar">
+    <div className={`progress-bar ${progressClass}`}>
       <animated.div
+        className="progress"
         style={{
           height: "100%",
           display: "flex",
           justifyContent: "flex-start",
-          backgroundColor: "#03424c",
+          backgroundColor: customBackgroundColor,
           borderRadius: "15px 0 0 15px",
           ...props,
         }}
