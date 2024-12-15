269e5c896083632c773b0ee806184d0791daddd6
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-12-16 01:53:16 +0330

    footer animation when hovered

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,45 +3,47 @@ import Switch from "react-switch";
 
 export function Footer({ isDarkMode, onToggle }) {
   return (
-    <div className="footer">
-      <div className="footer-links">
-        <a
-          href="https://github.com/MatinT-SA"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <AiFillGithub className="footer-icons" />
-          GitHub
-        </a>
-        <a
-          href="https://www.linkedin.com/in/matin-taherzadeh-sa/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <AiFillLinkedin className="footer-icons" />
-          LinkedIn
-        </a>
-      </div>
-      <div className="footer-copyright">
-        <p>
-          © 2024{" "}
+    <div className="footer-outer">
+      <div className="footer">
+        <div className="footer-links">
           <a
-            href="https://www.matintaherzadeh.ir/"
+            href="https://github.com/MatinT-SA"
             target="_blank"
-            rel="noreferrer noopener"
+            rel="noopener noreferrer"
           >
-            Matin Taherzadeh
+            <AiFillGithub className="footer-icons" />
+            GitHub
           </a>
-          . All Rights Reserved.
-        </p>
-      </div>
-      <div className="footer-theme-toggle">
-        <Switch
-          onChange={onToggle}
-          checked={isDarkMode}
-          uncheckedIcon={<div style={{ padding: "2px" }}>🌞</div>}
-          checkedIcon={<div style={{ padding: "2px" }}>🌙</div>}
-        />
+          <a
+            href="https://www.linkedin.com/in/matin-taherzadeh-sa/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillLinkedin className="footer-icons" />
+            LinkedIn
+          </a>
+        </div>
+        <div className="footer-copyright">
+          <p>
+            © 2024{" "}
+            <a
+              href="https://www.matintaherzadeh.ir/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Matin Taherzadeh
+            </a>
+            . All Rights Reserved.
+          </p>
+        </div>
+        <div className="footer-theme-toggle">
+          <Switch
+            onChange={onToggle}
+            checked={isDarkMode}
+            uncheckedIcon={<div style={{ padding: "2px" }}>🌞</div>}
+            checkedIcon={<div style={{ padding: "2px" }}>🌙</div>}
+          />
+        </div>
       </div>
     </div>
   );
